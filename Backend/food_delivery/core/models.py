e68d777867031fbad0ce4e07c2fa8b04336c3a00
Just Alex <just.alexdev@gmail.com>
2024-10-15 23:51:50 +0900

    Create models.py

---
+++ Backend/food_delivery/core/models.py
@@ -0,0 +1,43 @@
+# core/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+class News(models.Model):
+    image = models.ImageField(upload_to='news/')
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    ingredients = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    description = models.TextField()
+
+class Cart(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    favorite_products = models.ManyToManyField(Product, related_name='favorited_by')
+    order_history = models.ManyToManyField(Product, through='Order', related_name='orders')
+
+class Order(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    address = models.CharField(max_length=255)
