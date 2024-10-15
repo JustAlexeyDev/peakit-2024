1d5b1a1e4f7ca8790c7e46a34e3725561c2963aa
Just Alex <just.alexdev@gmail.com>
2024-10-15 23:51:48 +0900

    Create serializers.py

---
+++ Backend/food_delivery/core/serializers.py
@@ -0,0 +1,31 @@
+# core/serializers.py
+from rest_framework import serializers
+from .models import CustomUser, News, Product, Cart, Order
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'phone_number']
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'image']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price', 'ingredients', 'image', 'description']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'product', 'quantity', 'total_price', 'address']
+
+class CartSerializer(serializers.ModelSerializer):
+    favorite_products = ProductSerializer(many=True, read_only=True)
+    order_history = OrderSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'favorite_products', 'order_history']
