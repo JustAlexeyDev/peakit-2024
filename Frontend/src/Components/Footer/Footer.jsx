83edc58035eb5fa03126e826ec360d85050c74c3
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 22:19:12 +0900

    Update Footer.jsx

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,11 +1,32 @@
 import "./Footer.css";
 
+import { House, Utensils, ShoppingCart, Heart, History } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return(
-    <footer>
-      
-    </footer>
+    <div className="Footer--Coontainer">
+      <Link to="/" className="Footer--Item">
+        <House />
+        <p>Главная</p>
+      </Link>
+      <Link to="/MenuScreen" className="Footer--Item">
+        <Utensils />
+        <p>Меню</p>
+      </Link>
+      <Link to="/CartScreen" className="Footer--Item">
+        <ShoppingCart />
+        <p>Корзина</p>
+      </Link>
+      <Link to="/FavoritesScreen" className="Footer--Item">
+        <Heart />
+        <p>Избранное</p>
+      </Link>
+      <Link to="/HistoryScreen" className="Footer--Item">
+        <History />
+        <p>История</p>
+      </Link>
+    </div>
   );
 }
 export default Footer;
