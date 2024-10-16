732727b0e9347aa21863a15b2b8228cd246e3d6d
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 22:19:05 +0900

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import NotFoundScreen from "./Screens/NotFoundScreen";
 
+import Footer from "./Components/Footer/Footer";
+
 const App = () => {
   return(
-    <Routes>
-      <Route path="/*" element={<NotFoundScreen />}/>
-      <Route path="/" element={<HomeScreen />}/>
-      <Route path="/HomeScreen" element={<HomeScreen />}/>
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/*" element={<NotFoundScreen />}/>
+        <Route path="/" element={<HomeScreen />}/>
+        <Route path="/HomeScreen" element={<HomeScreen />}/>
+      </Routes> 
+      <Footer />     
+    </div>
   );
 }
 export default App;
