eaf6fa7fa7ffda4ab5ece9daede863b5dafc91af
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 08:49:41 +0900

    Create OrderConfirmationScreen.jsx

---
+++ Frontend/src/Screens/OrderConfirmationScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderConfirmationScreen = () => {
+  return (
+    <div>
+      <h2>Заказ успешно оформлен!</h2>
+      <p>Спасибо за ваш заказ. Мы свяжемся с вами в ближайшее время.</p>
+    </div>
+  );
+};
+
+export default OrderConfirmationScreen;
