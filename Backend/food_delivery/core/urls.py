9ec72b1fde34a3e6b17d45a20238069d75ec95b8
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 22:30:57 +0900

    Update urls.py

--- Backend/food_delivery/core/urls.py
+++ Backend/food_delivery/core/urls.py
@@ -1,13 +1,14 @@
 # core/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet, NewsViewSet, ProductViewSet, CartViewSet
+from .views import CustomUserViewSet, NewsViewSet, ProductViewSet, CartViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
 router.register(r'news', NewsViewSet)
 router.register(r'products', ProductViewSet)
 router.register(r'carts', CartViewSet)
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
