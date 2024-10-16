b604b84c2b15990f36a5dedec015d1ef1ed7a986
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 23:16:38 +0900

    Update urls.py

--- Backend/food_delivery/food_delivery/urls.py
+++ Backend/food_delivery/food_delivery/urls.py
@@ -1,4 +1,6 @@
 # food_delivery/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
@@ -21,6 +23,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
