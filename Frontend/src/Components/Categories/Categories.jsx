8141ed1d295758458370eff8410239f4447c54ff
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 23:56:38 +0900

    Update Categories.jsx

--- Frontend/src/Components/Categories/Categories.jsx
+++ Frontend/src/Components/Categories/Categories.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Если вы используете React Router
 import ip from "../../config";
 import "./Categories.css";
 
 const Categories = () => {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate(); // Инициализация useNavigate
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -22,18 +24,29 @@ const Categories = () => {
     fetchProducts();
   }, []);
 
+  const handleCategoryClick = (category) => {
+    navigate(`/MenuScreen?category=${encodeURIComponent(category)}`);
+  };
+
   return (
     <div className='Categories--Container'>
       <h2>Категории</h2>
       {products.length > 0 ? (
         <div className='Categories--Items'>
           {products.map((product) => (
-            <div key={product.id}>
-              <img className='Categories--Items__Image' src={product.image} alt={product.name || 'Product Image'} />
-            </div>
+            <button 
+              key={product.id} 
+              className='Categories--Items__Button' 
+              onClick={() => handleCategoryClick(product.name)}
+            >
+              <img 
+                className='Categories--Items__Image' 
+                src={product.image} 
+                alt={product.name || 'Product Image'} 
+              />
+            </button>
           ))}          
         </div>
-
       ) : (
         <p>Нет доступных категорий</p>
       )}
