3397021dc5fa15323b767d75e94be2b4044de5f2
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 22:30:53 +0900

    Update models.py

--- Backend/food_delivery/core/models.py
+++ Backend/food_delivery/core/models.py
@@ -23,12 +23,19 @@ class CustomUser(AbstractUser):
 class News(models.Model):
     image = models.ImageField(upload_to='news/')
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     ingredients = models.TextField()
     image = models.ImageField(upload_to='products/')
     description = models.TextField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
 
 class Cart(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
