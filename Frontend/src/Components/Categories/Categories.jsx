52183c09f0b85ca9f5392fdec6c9c25f5a73b76e
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-16 23:17:00 +0900

    Create Categories.jsx

---
+++ Frontend/src/Components/Categories/Categories.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import ip from "../../config";
+
+const Categories = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`${ip}/api/categories/`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setProducts(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Product Details</h1>
+      {products.length > 0 ? (
+        products.map((product) => (
+          <div key={product.id}>
+            <img src={product.image} alt={product.name || 'Product Image'} />
+          </div>
+        ))
+      ) : (
+        <p>Нет доступных категорий</p>
+      )}
+    </div>
+  );
+}
+
+export default Categories;
