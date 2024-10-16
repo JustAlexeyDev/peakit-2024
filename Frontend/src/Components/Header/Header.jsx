baee5039d1febc0e0e1c4216998d328bcbfb09dc
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 08:49:52 +0900

    Update Header.jsx

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
 import "./Header.css";
+import { Search, CircleUser } from "lucide-react";
 
-const Header = () => {
-  return(
-    <header>
-      
-    </header>
+const Header = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div className="Header--Container">
+      <div className="Header--Container__ProfileButton">
+        <button>
+          <CircleUser color="#fff" />
+        </button>
+      </div>
+      <div className="Header--Container__Search">
+        <form onSubmit={handleSearch}>
+          <input
+            type="search"
+            placeholder="Поиск.."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button type="submit">
+            <Search color="#808080" />
+          </button>
+        </form>
+      </div>
+    </div>
   );
-}
+};
+
 export default Header;
