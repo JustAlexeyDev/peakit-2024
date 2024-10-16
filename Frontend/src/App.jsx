3322ab2ead2f6c81214a66fbc39be105db3dd861
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 00:57:36 +0900

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import NotFoundScreen from "./Screens/NotFoundScreen";
 import MenuScreen from "./Screens/MenuScreen";
+import RegisterScreen from "./Screens/RegisterScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 import Footer from "./Components/Footer/Footer";
 
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/" element={<HomeScreen />}/>
         <Route path="/HomeScreen" element={<HomeScreen />}/>
         <Route path="/MenuScreen" element={<MenuScreen />}/>
+        <Route path="/RegisterScreen" element={<RegisterScreen />}/>
+        <Route path="/LoginScreen" element={<LoginScreen />}/>
       </Routes> 
       <Footer />     
     </div>
