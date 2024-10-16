c9a99130062929fd54ea12f45bdc2c2ea2c1afba
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 08:50:00 +0900

    Update urls.py

--- Backend/food_delivery/core/urls.py
+++ Backend/food_delivery/core/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, NewsViewSet, ProductViewSet, CartViewSet, CategoryViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
@@ -10,6 +12,8 @@ router.register(r'products', ProductViewSet)
 router.register(r'carts', CartViewSet)
 router.register(r'categories', CategoryViewSet)
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
