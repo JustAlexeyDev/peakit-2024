a64a00cbffb4ff08434251b37267453d0ec4c197
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 10:07:04 +0900

    Update ProductDetailScreen.jsx

--- Frontend/src/Screens/ProductDetailScreen.jsx
+++ Frontend/src/Screens/ProductDetailScreen.jsx
@@ -1,6 +1,7 @@
 // ProductDetailScreen.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import "./Styles/ProductDetailScreen.css";
 
 const ProductDetailScreen = ({ products }) => {
   const { productId } = useParams();
@@ -11,11 +12,30 @@ const ProductDetailScreen = ({ products }) => {
   }
 
   return (
-    <div className="product-detail">
-      <h2>{product.name}</h2>
+    <div className="Product--Detail">
       <img src={product.image} alt={product.name} className="product-image" />
-      <p>{product.description}</p>
-      <p>Цена: {product.price} ₽</p>
+      
+      <div className='Product--InfoContainer'>
+        <div className='Product--InfoContainer__Header'>
+          <div>
+            <h2>{product.name}</h2>
+            <p>{product.category.name}</p>          
+          </div>
+          <span>
+            <p>{product.price} ₽</p>
+          </span>
+        </div>
+
+        <div className='Product--InfoContainer__OtherInfo'>
+          <div>{product.description}</div>  
+          <h3>Ингридиенты</h3>
+          <div>
+            <p>{product.ingredients}</p>
+          </div>      
+        </div>
+
+      </div>
+
       {/* Дополнительные детали о продукте */}
     </div>
   );
