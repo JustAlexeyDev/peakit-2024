045f9bad0289ee72b0ad1a713b2b3ba73a033820
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 10:33:59 +0900

    Update urls.py

--- Backend/food_delivery/food_delivery/urls.py
+++ Backend/food_delivery/food_delivery/urls.py
@@ -7,6 +7,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Food Delivery API",
@@ -21,10 +22,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+   path('admin/', admin.site.urls),
+   path('api/', include('core.urls')),
    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
