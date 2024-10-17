a64d771357543acadd2df06b57adc9a4d762c408
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-17 12:08:08 +0900

    Update CartScreen.jsx

--- Frontend/src/Screens/CartScreen.jsx
+++ Frontend/src/Screens/CartScreen.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Styles/CartScreen.css";
 
@@ -10,31 +9,41 @@ const CartScreen = ({ cart, products, updateQuantity, removeFromCart, totalPrice
   };
 
   return (
-    <div className="cart-screen">
+    <div className="menu-screen">
       <h2>Корзина</h2>
       {Object.keys(cart).length > 0 ? (
-        <div className="cart-items">
+        <div className="product-list">
           {Object.keys(cart).map((productId) => {
             const product = products.find(p => p.id === parseInt(productId));
             if (!product) return null;
             return (
-              <div key={product.id} className="cart-item">
-                <div className="cart-item-image">
-                  <img src={product.image} alt={product.name} />
-                </div>
-                <div className="cart-item-details">
-                  <h3>{product.name}</h3>
-                  <div className='cart-item-buttons'>
-                    <div className="cart-item-controls">
-                      <button onClick={() => updateQuantity(product.id, -1)}>-</button>
-                      <span>{cart[product.id]} шт</span>
-                      <button onClick={() => updateQuantity(product.id, 1)}>+</button>
+              <div onClick={() => viewProductDetails(product.id)} key={product.id} className="product-card">
+                <img src={product.image} alt={product.name} className="product-image" />
+                <div className='product--description'>
+                  <div>
+                    <h3>{product.name}</h3>
+                    <p>{product.description}</p>                
+                  </div> 
+                  {cart[product.id] ? (
+                    <div className="cart-controls">
+                      <div className='product--buy--button'>
+                        <button onClick={(e) => { e.stopPropagation(); updateQuantity(product.id, -1); }}>-</button>
+                        <span>{cart[product.id]} шт</span>
+                        <button onClick={(e) => { e.stopPropagation(); updateQuantity(product.id, 1); }}>+</button>
+                      </div>
+                      <div className='product--buy--button__remove'>
+                        <button onClick={(e) => { e.stopPropagation(); removeFromCart(product.id); }}>
+                          Удалить
+                          <p>({cart[product.id] * product.price} ₽)</p>  
+                        </button>
+                      </div>
                     </div>
-                    <button className='cart-remove' onClick={() => removeFromCart(product.id)}>
-                      Удалить
-                      <p>Сумма: {cart[product.id] * product.price} ₽</p>
+                  ) : (
+                    <button className='product--buy--button' onClick={(e) => { e.stopPropagation(); addToCart(product.id); }}>
+                      <b>В Корзину </b>
+                      <p>{product.price} ₽</p>
                     </button>
-                  </div>
+                  )}
                 </div>
               </div>
             );
